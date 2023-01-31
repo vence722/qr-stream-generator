d9df1bd869f5c500a31e302ee0da504a291222bc
laurence <laurence0636@outlook.com>
2023-01-31 11:27:18 +0800

    init

---
+++ README.txt
@@ -0,0 +1,7 @@
+1. Run the below command line to install python package requirement:
+pip install -r requirements.txt
+
+2. run the main script:
+python main.py
+
+3. check the staging/stg_e/20230131_1058 to get the list of QR images
---
+++ main.py
@@ -0,0 +1,54 @@
+# %%
+import qrcode
+import zlib
+import os
+import tqdm
+config = {}
+config["is_zip"] = False
+config["chunk_str_size"] = 100
+config["output_dir"] = "staging/stg_e/20230131_1058"
+
+# %%
+
+
+if __name__ == '__main__':
+    if not os.path.exists(config["output_dir"]):
+        os.makedirs(config["output_dir"])
+
+
+    in_str = '''
+        def get_tr_iter(train_data, FLAGS, NodeMinibatchIterator):
+            G = train_data[0]
+            features = train_data[1]
+            id_map = train_data[2]
+            class_map  = train_data[4]
+            if isinstance(list(class_map.values())[0], list):
+                num_classes = len(list(class_map.values())[0])
+            else:
+                num_classes = len(set(class_map.values()))
+            context_pairs = train_data[3] if FLAGS.random_context else None
+            placeholders = {
+                'labels' : tf.cast(tf.compat.v1.distributions.Bernoulli(probs=0.7).sample(sample_shape=(1, num_classes)), tf.float32),
+                'batch' : tf.constant(list(G.nodes)[:1], dtype=tf.int32, name='batch1'),
+                'dropout': tf.constant(0., dtype=tf.float32, name='batch1'),
+                'batch_size' : tf.constant(FLAGS.batch_size, dtype=tf.float32, name='batch1'),
+            }
+            minibatch = NodeMinibatchIterator(G,
+                    id_map,
+                    placeholders, 
+                    class_map,
+                    num_classes,
+                    batch_size=FLAGS.batch_size,
+                    max_degree=FLAGS.max_degree, 
+                    context_pairs = context_pairs)
+            return minibatch
+        '''
+    if config["is_zip"]:
+        in_str = zlib.compress(in_str.encode('utf-8'))
+
+    
+    chunk_str_size = config["chunk_str_size"]
+    for i in tqdm.tqdm(range(round(len(in_str) /  chunk_str_size + 0.5))):
+        img = qrcode.make(f"[TIME_TOKEN_{i}]\n" + in_str[i * chunk_str_size : (i + 1) * chunk_str_size])
+        img.save(f'{config["output_dir"]}/out_{i}.png')
+
